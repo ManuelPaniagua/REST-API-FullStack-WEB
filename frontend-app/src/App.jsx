a257c588019b6c6d4b9af2f8ba86cfa6fa6ccb48
Manuel <m.p.porroa@maxedv-beratung.de>
2024-07-01 13:39:16 +0200

    create pages register, login and install react hook form

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -5,6 +5,9 @@ import Home from './pages/Home';
 import CreateTasks from './pages/CreateTasks';
 import DeleteTask from './pages/DeleteTask';
 import EditTask from './pages/EditTask';
+import { RegisterPage } from './pages/RegisterPage';
+import { LoginPage } from './pages/LoginPage';
+
 const App = () => {
   return (
     <Routes>
@@ -12,6 +15,8 @@ const App = () => {
       <Route path='/task/new' element={<CreateTasks />}></Route>
       <Route path='/task/delete/:id' element={<DeleteTask />}></Route>
       <Route path='/task/edit/:id' element={<EditTask />}></Route>
+      <Route path='/register' element={<RegisterPage />}></Route>
+      <Route path='/login' element={<LoginPage />}></Route>
     </Routes>
   );
 };
