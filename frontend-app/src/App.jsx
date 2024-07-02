6a7174ff67f4a0d8186c2b730c86f080abf3d4ab
Manuel <m.p.porroa@maxedv-beratung.de>
2024-07-02 10:49:24 +0200

    profile route

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -5,23 +5,26 @@ import Home from './pages/Home';
 import CreateTasks from './pages/CreateTasks';
 import DeleteTask from './pages/DeleteTask';
 import EditTask from './pages/EditTask';
-import  RegisterPage from './pages/RegisterPage';
-import  LoginPage  from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 import { AuthProvider } from './context/AuthContext';
+import ProfilePage from './pages/ProfilePage';
 
 const App = () => {
-  return (
-    <AuthProvider>
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/task/new' element={<CreateTasks />}></Route>
-      <Route path='/task/delete/:id' element={<DeleteTask />}></Route>
-      <Route path='/task/edit/:id' element={<EditTask />}></Route>
-      <Route path='/register' element={<RegisterPage />}></Route>
-      <Route path='/login' element={<LoginPage />}></Route>
-    </Routes>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/register' element={<RegisterPage />}></Route>
+                <Route path='/login' element={<LoginPage />}></Route>
+
+                <Route path='/task/new' element={<CreateTasks />}></Route>
+                <Route path='/task/delete/:id' element={<DeleteTask />}></Route>
+                <Route path='/task/edit/:id' element={<EditTask />}></Route>
+                <Route path='/profile' element={<ProfilePage />}></Route>
+            </Routes>
+        </AuthProvider>
+    );
 };
 
 export default App;
